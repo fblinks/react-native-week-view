baee38e73696efb0f70a5c9431e4e7ffd3a26c7b
Fabrizio Bertone <52320328+fblinks@users.noreply.github.com>
2021-12-09 09:27:58 +0100

    Update Events.styles.js

--- src/Events/Events.styles.js
+++ src/Events/Events.styles.js
@@ -1,7 +1,7 @@
 import { StyleSheet } from 'react-native';
 import { CONTAINER_WIDTH, CONTENT_OFFSET } from '../utils';
 
-const GREY_COLOR = '#E9EDF0';
+const GREY_COLOR = '#d1d7da';
 
 const styles = StyleSheet.create({
   container: {
